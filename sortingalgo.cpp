7b03a0c4f74b8b18c95fa919625f92797bf63c5c
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-07-09 17:14:15 +0530

    added bubble sort

--- sortingalgo.cpp
+++ sortingalgo.cpp
@@ -18,6 +18,21 @@ void selectionSort(int arr[], int n)
     }
 }
 
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i-1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 int main()
 {
     int n;
@@ -26,7 +41,8 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    selectionSort(a, n);
+    // selectionSort(a, n);
+    bubbleSort(a, n);
 
     for (int i = 0; i < n; i++)
         cout << a[i] << " ";
