1e5293e843b0d1e4ecb1e38cde472dbc66f62908
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-07-09 18:09:29 +0530

    added bubble sort more efficient

--- sortingalgo.cpp
+++ sortingalgo.cpp
@@ -22,6 +22,7 @@ void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
+        int didswap = 0;
         for (int j = 0; j < n - i-1; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -29,8 +30,12 @@ void bubbleSort(int arr[], int n)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                didswap = 1;
             }
         }
+
+        if(didswap == 0)
+            break;
     }
 }
 int main()
