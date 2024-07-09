8305f1c83805b6ccc2344375c656d2f25d53409d
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-07-09 23:35:55 +0530

    added merge sort

--- sortingalgo.cpp
+++ sortingalgo.cpp
@@ -53,6 +53,54 @@ void insertionSort(int arr[], int n)
         }
     }
 }
+
+void merge(int arr[], int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid+1;
+    while(left<=mid && right<=high)
+    {
+        if(arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left<=mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right<=high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i =low; i<=high;i++)
+        arr[i] = temp[i-low];
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    if(low >= high) return;
+
+    int mid = (low+high)/2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+
+    merge(arr, low, mid, high);
+
+}
 int main()
 {
     int n;
@@ -63,7 +111,8 @@ int main()
 
     // selectionSort(a, n);
     //bubbleSort(a, n);
-    insertionSort(a,n);
+    //insertionSort(a,n);
+    mergeSort(a, 0, n-1);
 
     for (int i = 0; i < n; i++)
         cout << a[i] << " ";
