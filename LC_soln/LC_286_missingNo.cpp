53c81ca4806bdec16ee322ec2cc109168a5cbb99
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-07-12 09:14:18 +0530

    problem 286 optimal solution

--- LC_soln/LC_286_missingNo.cpp
+++ LC_soln/LC_286_missingNo.cpp
@@ -1,7 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int missingNumber(vector<int>& nums) {
+int missingNumberBrute(vector<int>& nums) {
+        int n = nums.size();
+        for(int i =0; i<=n;i++)
+        {
+            int flag = 0;
+            for(int j =0; j<n ;j++){
+                if(nums[j] == i)
+                {
+                    flag =1;
+                    break;
+                }
+            }
+
+            if(flag == 0) return i;
+
+        }
+        return -1;
+    }
+
+
+int missingNumberBetter(vector<int>& nums) {
         int n = nums.size();
         vector<int> hashArr(n+1);
 
@@ -22,6 +42,19 @@ int missingNumber(vector<int>& nums) {
     return -1;
 }
 
+
+int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum1 = (n*(n+1))/2;
+        int sum2=0;
+        for(int i =0; i<n;i++)
+        {
+           sum2 += nums[i];
+        }  
+
+        return sum1-sum2;    
+}
 
 int main()
 {
