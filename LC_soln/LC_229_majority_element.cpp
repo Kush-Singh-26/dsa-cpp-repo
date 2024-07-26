5087b55bc9cb8c7357c510d2ea3bf568352e7d4b
kushsingh2004+webdev@gmail.com <kushsingh2004+webdev@gmail.com>
2024-07-26 13:32:21 +0530

    problem 229 using moores voting algorithm majortiy element greater than n/3

--- LC_soln/LC_229_majority_element.cpp
+++ LC_soln/LC_229_majority_element.cpp
@@ -20,6 +20,51 @@ vector<int> majorityElement(vector<int>& nums) {
 
         return ans;
     }
+
+vector<int> majorityElement_optimal(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        int ele1 = INT_MIN, ele2 = INT_MIN, count1 =0, count2 =0;
+
+        for(int i =0;i<n;i++)
+        {
+            if(count1 == 0 && nums[i] != ele2)
+            {
+                ele1 = nums[i];
+                count1++;
+            }
+            
+            else if(count2 == 0 && nums[i] != ele1)
+            {
+                ele2 = nums[i];
+                count2++;
+            }
+
+            else if(nums[i] == ele1) count1++;
+
+            else if(nums[i] == ele2) count2++;
+
+            else 
+                {
+                    count1--; count2--;
+                }
+        }
+
+        count1 =0; count2 =0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] == ele1) count1++;
+            if(nums[i] == ele2) count2++;
+        }
+
+        int limit = n/3;
+        if(count1 > limit) ans.push_back(ele1);
+        if(count2 > limit) ans.push_back(ele2);
+ 
+       
+        return ans;
+    }
 int main() {
     int n;
     cin >> n;
