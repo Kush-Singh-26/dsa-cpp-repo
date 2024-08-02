f2817d5ee6474c4248b8532dc7ecb6e2e4a73276
kushsingh2004+webdev@gmail.com <kushsingh2004+webdev@gmail.com>
2024-08-02 11:48:47 +0530

    lc problem 81 search in rotated sorted array with duplicate elements part 2

---
+++ LC_soln/LC_81_search_in_roated_sorted_array_2.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l =0,h= n-1,mid;
+        while(l<=h)
+        {
+            mid = (l+h)/2;
+            if(nums[mid] == target) return 1;
+            if(nums[l] == nums[mid] && nums[mid] == nums[h])
+            {
+                l++;
+                h--;
+                continue;
+            }
+            if(nums[l] <= nums[mid])
+            {
+                if(nums[l] <= target && target <= nums[mid])
+                    h = mid-1;
+                else 
+                    l = mid+1;
+            }
+
+            else
+            {
+                if(nums[mid] <= target && target <= nums[h])
+                    l = mid+1;
+                else
+                    h = mid-1;
+            }
+        }
+
+        return 0;
+    }
+int main()
+{
+    int n;
+    cin >> n;
+
+    int target;
+    cin >> target;
+    vector <int> a(n);
+
+    for(int i=0; i<n;i++) 
+        cin >> a[i];
+
+    cout << search(a,target);
+
+    return 0;
+}
